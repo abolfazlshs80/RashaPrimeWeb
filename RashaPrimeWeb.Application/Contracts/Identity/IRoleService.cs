60bf9fa32ab7aadff8ba5296818433d29e1c8fbf
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-18 21:24:15 +0330

    Update IRoleService

--- RashaPrimeWeb.Application/Contracts/Identity/IRoleService.cs
+++ RashaPrimeWeb.Application/Contracts/Identity/IRoleService.cs
@@ -1,13 +1,15 @@
-﻿namespace RashaPrimeWeb.Application.Contracts.Identity
+﻿using RashaPrimeWeb.Application.Models.Identity.Role;
+
+namespace RashaPrimeWeb.Application.Contracts.Identity
 {
     public interface IRoleService
     {
-        public Task<ResponseDto> AddRoleToUsers(AddRoleToUserDto model);
-        public Task<ResponseDto> DeleteRoleFromUsers(AddRoleToUserDto model);
-        public Task<ResponseDto> AddRole(AddRoleDto model);
-        public Task<ResponseDto> UpdateRole(AddRoleDto model);
-        public Task<ResponseDto> DeleteRoleRole(string RoleId);
-        public Task<ResponseDto> IsInRole(AddRoleToUserDto model);
+        public Task<ErrorOr<bool>> AddRoleToUsers(AddRoleToUserDto model);
+        public Task<ErrorOr<bool>> DeleteRoleFromUsers(AddRoleToUserDto model);
+        public Task<ErrorOr<bool>> AddRole(AddRoleDto model);
+        public Task<ErrorOr<bool>> UpdateRole(AddRoleDto model);
+        public Task<ErrorOr<bool>> DeleteRoleRole(string RoleId);
+        public Task<ErrorOr<bool>> IsInRole(AddRoleToUserDto model);
         public Task <IEnumerable< RolesDto>> GetRoles();
         public Task <IEnumerable<AddRoleToUserDto>> GetRoles(string id);
         public Task < RolesDto> GetRole(string roleId);
