154fd20e59b5165974d680e8ff0c95ba94f4bb54
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-18 17:59:35 +0330

    Add Email Model

---
+++ RashaPrimeWeb.Application/Models/Infrastructure/EmailSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RashaPrimeWeb.Application.Models.Infrastructure
+{
+    public class EmailSettings
+    {
+        public string ApiKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
+    }
+}
