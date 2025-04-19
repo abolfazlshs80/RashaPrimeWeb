c4fec26e4f57f76bf4175508e6c09924e418add7
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-19 11:45:35 +0330

    update file service

---
+++ RashaPrimeWeb.Application/Contracts/Infrastructure/IFileUploaderService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RashaPrimeWeb.Application.Contracts.Infrastructure
+{
+    public interface IFileUploaderService
+    {
+
+        Task<string> UploadFile(Stream fileStream, string directoryName, string fileName);
+        Task<string> UpdloadFile(IFormFile file, string directoryName,  string Name);
+        Task DeleteFile(string directoryName, string Name);
+        string DownloadFile(string filename, string type);
+
+    }
+}
