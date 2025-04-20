76c9ab75d47886742c62e3a6e521d2ed909d2c79
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-20 10:29:31 +0330

    update link

---
+++ RashaPrimeWeb.Presentation/Components/Handler/MainLayout/HeaderComponents.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Host;
+using Microsoft.Extensions.Localization;
+
+
+namespace RashaPrimeWeb.WEB.Components.Main
+{
+    public class HeaderComponents : ViewComponent
+    {
+      //  private readonly IStringLocalizer _localizer;
+        //private readonly IMenuService _menuService;
+        //private readonly ITagService _tagService;
+        //private readonly ICategoryService _categoryService;
+        //private readonly IBlogService _blogService;
+        //private readonly INewsService _newsService;
+        //private readonly IServiceService _serviceService;
+        //private readonly ISettingService _SettingService;
+        //private readonly ILanguagesService _languageService;
+
+        //private readonly IMapper _mapper;
+
+        //public HeaderComponents()
+        //{
+        
+        //}
+      
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var Header = new HeaderVM();
+            //Header.Setting = await _SettingService.GetCurrentSettingActive();
+            //Header.Menu = await _menuService.GetMenuForHeaderActive();
+            //Header.Categories = await _categoryService.GetCategoryForHeaderActive();
+            //Header.Blogs = await _blogService.GetBlogForHeaderActive();
+            //Header.News = await _newsService.GetNewsForHeaderActive();
+            //Header.Service = await _serviceService.GetServiceForHeaderActive();
+            //Header.Tag = await _tagService.GetTagForHeaderActive();
+            //Header.Languages = await _languageService.GetLanguagesLast();
+            return View("/Components/Views/MainLayout/Header.cshtml");
+        }
+
+    }
+}
