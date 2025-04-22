d0e3942da48e15bddc386e64bfd5b68dc0d23444
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-22 09:37:42 +0330

    Add Area

--- RashaPrimeWeb.Presentation/Program.cs
+++ RashaPrimeWeb.Presentation/Program.cs
@@ -35,10 +35,20 @@ namespace RashaPrimeWeb.Presentation
             app.UseAuthentication(); // قبل از UseAuthorization قرار دارد
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+    
             app.Run();
         }
     }
