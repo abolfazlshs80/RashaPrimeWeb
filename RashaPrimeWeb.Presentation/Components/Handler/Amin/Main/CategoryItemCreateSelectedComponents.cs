a47baec6b0a1186382713069a3b561ff1db07815
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-29 10:11:02 +0330

    Add Component SelectedCategory

---
+++ RashaPrimeWeb.Presentation/Components/Handler/Amin/Main/CategoryItemCreateSelectedComponents.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RashaPrimeWeb.Application.CQRS.Category.Queries.GetAllCategory;
+using RashaPrimeWeb.Application.Models.ViewModel.Admin.HtmlHelper;
+
+
+namespace Kashi_Seramic.MVC.Components.Admin.Category
+{
+    public class CategoryItemCreateSelectedComponents(IMediator mediator) : ViewComponent
+    {
+    
+        public async Task<IViewComponentResult> InvokeAsync(List<int>  cates)
+        {
+
+            int Take = 5;
+            int Page = 1;
+            bool GetOldest = false;
+            var query = new GetAllCategoryQuery(null, GetOldest, Page, Take);
+            var res_query = await mediator.Send(query);
+            var model = new ListDropDownCategory();
+            model.Category = res_query.Items;
+            model.CategoryActive = cates;
+            return View("/Components/Views/Admin/Main/CategoryItemCreateSelected.cshtml", model);
+        }
+
+    }
+}
+
+
