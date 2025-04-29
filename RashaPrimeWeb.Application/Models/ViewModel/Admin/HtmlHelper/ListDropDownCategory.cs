99ccacf39afeb1b54c0999fc550a774491db8551
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-29 10:13:05 +0330

    add model listdropdown category

---
+++ RashaPrimeWeb.Application/Models/ViewModel/Admin/HtmlHelper/ListDropDownCategory.cs
@@ -0,0 +1,17 @@
+﻿using RashaPrimeWeb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RashaPrimeWeb.Application.CQRS.Category.Queries.GetAllCategory;
+
+namespace RashaPrimeWeb.Application.Models.ViewModel.Admin.HtmlHelper
+{
+
+    public class ListDropDownCategory
+    {
+        public IEnumerable<GetAllCategoryDto> Category { get; set; }
+        public IEnumerable<int> CategoryActive { get; set; }
+    }
+}
